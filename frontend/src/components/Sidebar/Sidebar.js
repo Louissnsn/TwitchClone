95f8a7e0a30ac198f600eb48ac09d85e0b5b9b50
Louissnsn <138105116+Louissnsn@users.noreply.github.com>
2024-05-23 12:07:35 +0200

    create front + back dir, install express

---
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <div className="sideBar">
+      <h2 className="titreSideBar">Chaînes recommandées</h2>
+      <ul className="listeStream"></ul>
+    </div>
+  );
+}
